10975cddf644acb0cfdffd80e0ffb3c43acde9e5
Keith Gould <keithmgould@gmail.com>
2014-05-21 17:36:35 -0400

    gracefully shut down

--- lib/resque/plugins/resque_heroku_autoscaler.rb
+++ lib/resque/plugins/resque_heroku_autoscaler.rb
@@ -8,6 +8,14 @@ module Resque
         Resque::Plugins::HerokuAutoscaler::Config
       end
 
+      def self.config
+        yield Resque::Plugins::HerokuAutoscaler::Config
+      end
+
+      def heroku_api
+        @heroku_api ||= ::Heroku::API.new(api_key: config.heroku_api_key)
+      end
+
       def queue
         self.instance_variable_get("@queue")
       end
@@ -20,7 +28,7 @@ module Resque
         Resque.size(queue)
       end
 
-      def after_enqueue_scale_workers_up(*args)
+      def after_enqueue_scale_workers(*args)
         scale_on_enqueue unless config.scaling_disabled?
       end
 
@@ -32,22 +40,31 @@ module Resque
         scale unless config.scaling_disabled?
       end
 
-      def set_workers(workers_needed_count)
-        return if workers_needed_count == current_worker_count
+      def add_workers(workers_needed_count)
         heroku_api.post_ps_scale(config.heroku_app, worker_name, workers_needed_count)
-        Resque.redis.set("last_scaled_for_#{queue}", Time.now)
+      end
+
+      # lets process finish gracefully:
+      # http://rubydoc.info/github/defunkt/resque/Resque/Worker:shutdown
+      def remove_workers(workers_to_remove_count)
+        workers_to_remove = current_workers.first(workers_to_remove_count)
+        workers_to_remove.each { |worker| worker.shutdown }
       end
 
       def scale
         return unless time_to_scale?
-        new_count = config.new_worker_count(pending_count)
-        set_workers(new_count)
-      end
+        new_workers_count     = config.new_worker_count(pending_count)
+        current_workers_count = current_workers.count
 
-      def initialize_timer
-        unless Resque.redis.get("last_scaled_for_#{queue}")
-          Resque.redis.set("last_scaled_for_#{queue}", Time.now)
+        if new_workers_count == current_workers_count
+          return
+        elsif new_workers_count > current_workers_count
+          add_workers(new_workers_count)
+        else
+          remove_workers(current_workers_count - new_workers_count)
         end
+
+        set_last_scaled_time
       end
 
       def scale_on_enqueue
@@ -55,16 +72,18 @@ module Resque
         scale
       end
 
-      def heroku_api
-        @heroku_api ||= ::Heroku::API.new(api_key: config.heroku_api_key)
+      def set_last_scaled_time
+        Resque.redis.set("last_scaled_for_#{queue}", Time.now)
       end
 
-      def self.config
-        yield Resque::Plugins::HerokuAutoscaler::Config
+      def initialize_timer
+        unless Resque.redis.get("last_scaled_for_#{queue}")
+          set_last_scaled_time
+        end
       end
 
-      def current_worker_count
-        Resque.workers.map(&:to_s).count { |p| p.split(":")[2] == queue.to_s  }
+      def current_workers
+        Resque.workers.map(&:to_s).select { |p| p.split(":")[2] == queue.to_s  }
       end
 
       def time_to_scale?
@@ -73,14 +92,6 @@ module Resque
 
         time_waited_so_far = Time.now - Time.parse(last_scaled)
         time_waited_so_far >=  config.wait_time || time_waited_so_far < 0
-      end
-
-      def log(message)
-        if defined?(Rails)
-          Rails.logger.info(message)
-        else
-          puts message
-        end
       end
     end
   end
